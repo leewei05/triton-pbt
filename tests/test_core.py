68186a7ee320a13c8f777bd79e4788d731c78c01
Lee Wei <lee10202013@gmail.com>
2026-02-19 15:27:42 -0700

    Create test core file

---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+import triton
+import triton.language as tl
+from hypothesis import given, strategies as st, settings
+
+# ----------------
+# data types
+# ----------------
+int_dtypes = ['int8', 'int16', 'int32', 'int64']
+uint_dtypes = ['uint8', 'uint16', 'uint32', 'uint64']
+integral_dtypes = int_dtypes + uint_dtypes
+float_dtypes = ['float16', 'float32', 'float64']
+float_dtypes_with_bfloat16 = float_dtypes + ['bfloat16']
+dtypes = integral_dtypes + float_dtypes
+dtypes_with_bfloat16 = dtypes + ['bfloat16']
+
+if __name__ == "__main__":
+    print("Starting Hypothesis tests...")
