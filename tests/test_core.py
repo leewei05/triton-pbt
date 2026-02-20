1997b3fed421b4e5c2efae2350550d823043d80b
Lee Wei <lee10202013@gmail.com>
2026-02-19 17:06:33 -0700

    Use torch type

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,13 +7,20 @@ import numpy as np
 # ----------------
 # data types
 # ----------------
-int_dtypes = ['int8', 'int16', 'int32', 'int64']
-uint_dtypes = ['uint8', 'uint16', 'uint32', 'uint64']
+int_dtypes = [torch.int8, torch.int16, torch.int32, torch.int64]
+uint_dtypes = [torch.uint8]  # Note: PyTorch currently only supports uint8
 integral_dtypes = int_dtypes + uint_dtypes
-float_dtypes = ['float16', 'float32', 'float64']
-float_dtypes_with_bfloat16 = float_dtypes + ['bfloat16']
+
+float_dtypes = [torch.float16, torch.float32, torch.float64]
+float_dtypes_with_bfloat16 = float_dtypes + [torch.bfloat16]
+
 dtypes = integral_dtypes + float_dtypes
-dtypes_with_bfloat16 = dtypes + ['bfloat16']
+dtypes_with_bfloat16 = dtypes + [torch.bfloat16]
+
+# A GPU executes instructions in groups of 32 threads called a Warp
+# 4 * 32, 8 * 32 ...
+BLOCK_SIZES = [128, 256, 512, 1024]
+NUM_WARPS = [4, 8, 16]
 
 # ----------------
 # test math ops
@@ -35,34 +42,42 @@ def abs_kernel(x_ptr, z_ptr, n_elements, size: tl.constexpr):
 @given(
     # random tensor sizes
     n=st.integers(min_value=1, max_value=4096),
+    block_size=st.sampled_from(BLOCK_SIZES),
+    num_warps=st.sampled_from(NUM_WARPS),
+    # all data types
+    dtype=st.sampled_from(dtypes)
 )
-def test_abs(n):
-    dtype=torch.float32
+def test_abs(n, block_size, num_warps, dtype):
     device = 'cuda'
-    BLOCK_SIZE = 128
     
     # inputs
-    x_np = np.random.uniform(-10, 10, size=(n,)).astype(np.float32)
-    x_torch = torch.from_numpy(x_np).to(device)
+    if dtype in float_dtypes_with_bfloat16:
+        x_torch = torch.randn(n, device=device, dtype=dtype)
+    elif dtype == torch.uint8:
+        # Unsigned cannot be negative; 0 to 255 is the valid range
+        x_torch = torch.randint(0, 255, (n,), device=device, dtype=dtype)
+    else:
+        x_torch = torch.randint(-100, 100, (n,), device=device, dtype=dtype)
+
     z_torch = torch.empty_like(x_torch)
 
     # reference result 
-    z_ref = np.abs(x_np)
+    z_ref = torch.abs(x_torch)
 
     # We need enough blocks to cover 'n' elements
     # If we have 130 elements, Triton will schedule 2 blocks.
     # The first block handles 0 - 127 elements, the rest in the second block.
-    grid = (triton.cdiv(n, BLOCK_SIZE),)
+    grid = (triton.cdiv(n, block_size),)
     abs_kernel[grid](
         x_ptr=x_torch, 
         z_ptr=z_torch, 
         n_elements=n,
-        size=BLOCK_SIZE,
-        num_warps=4
+        size=block_size,
+        num_warps=num_warps
     )
 
-    z_tri_np = z_torch.cpu().numpy()
-    np.testing.assert_allclose(z_ref, z_tri_np, rtol=1e-5)
+    tol = 1e-2 if dtype in [torch.float16, torch.bfloat16] else 1e-5
+    torch.testing.assert_close(z_torch, z_ref, rtol=tol, atol=tol)
 
 if __name__ == "__main__":
     print(f"GPU: {torch.cuda.get_device_name(0)}")
