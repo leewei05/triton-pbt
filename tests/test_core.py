1a7c8f6c0ade5242b0d1d6441760a352affe754f
Lee Wei <lee10202013@gmail.com>
2026-02-26 12:11:36 -0700

    Test binary divide for integer types

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,7 @@
 import torch
 import triton
 import triton.language as tl
-from hypothesis import given, strategies as st, settings
+from hypothesis import given, assume, strategies as st, settings
 import pytest
 
 # ----------------
@@ -148,6 +148,14 @@ def binary_kernel(x_ptr, y_ptr, z_ptr, n_elements, size: tl.constexpr, op_name: 
         z = x - y
     elif op_name == "*":
         z = x * y
+    elif op_name == "/":
+        # Triton promotes 16-bit floating-point / and % to 32-bit because there
+        # are no native div or FRem operations on float16. Since we have to
+        # convert anyway, we may as well take the accuracy bump.
+        if x.dtype == tl.float16 or x.dtype == tl.bfloat16:
+            z = x.to(tl.float32) / y.to(tl.float32)
+        else:
+            z = x / y
 
     tl.store(z_ptr + offsets, z, mask=mask)
 
@@ -155,6 +163,8 @@ BINARY_OP_CONFIGS = {
     "+": (torch.add, dtypes_with_bfloat16),
     "-": (torch.sub, dtypes_with_bfloat16),
     "*": (torch.mul, dtypes_with_bfloat16),
+    # TODO: test floating point numbers
+    "/": (torch.div, integral_dtypes),
 }
 
 @pytest.mark.parametrize("op_name", list(BINARY_OP_CONFIGS.keys()))
@@ -168,29 +178,59 @@ BINARY_OP_CONFIGS = {
 def test_binary(n, block_size, num_warps, op_name, data):
     ref_func, allowed_dtypes = BINARY_OP_CONFIGS[op_name]
     device = 'cuda'
-    dtype = data.draw(st.sampled_from(allowed_dtypes))
 
-    if dtype in float_dtypes_with_bfloat16:
-        x_torch = torch.randn(n, device=device, dtype=dtype)
-        y_torch = torch.randn(n, device=device, dtype=dtype)
-    elif dtype == torch.uint8:
-        x_torch = torch.randint(0, 255, (n,), device=device, dtype=dtype)
-        y_torch = torch.randint(0, 255, (n,), device=device, dtype=dtype)
-    else:
-        x_torch = torch.randint(-100, 100, (n,), device=device, dtype=dtype)
-        y_torch = torch.randint(-100, 100, (n,), device=device, dtype=dtype)
+    dtype_x = data.draw(st.sampled_from(allowed_dtypes))
+    dtype_y = data.draw(st.sampled_from(allowed_dtypes))
 
-    z_torch = torch.empty_like(x_torch)
+    is_u_x = (dtype_x == torch.uint8)
+    is_u_y = (dtype_y == torch.uint8)
+    is_int_x = dtype_x in int_dtypes
+    is_int_y = dtype_y in int_dtypes
+
+    mixed_signedness = (is_u_x and is_int_y) or (is_int_x and is_u_y)
+    should_fail = op_name == "/" and mixed_signedness
+
+    def gen_data(dtype):
+        if dtype in float_dtypes_with_bfloat16:
+            return torch.randn(n, device=device, dtype=dtype)
+        elif dtype == torch.uint8:
+            return torch.randint(0, 255, (n,), device=device, dtype=dtype)
+        else:
+            return torch.randint(-100, 100, (n,), device=device, dtype=dtype)
+
+    x_torch = gen_data(dtype_x)
+    y_torch = gen_data(dtype_y)
+
     z_ref = ref_func(x_torch, y_torch)
+    z_torch = torch.empty_like(z_ref)
 
     grid = (triton.cdiv(n, block_size),)
-    binary_kernel[grid](
-        x_ptr=x_torch, y_ptr=y_torch, z_ptr=z_torch,
-        n_elements=n, size=block_size, op_name=op_name, num_warps=num_warps
-    )
 
-    tol = 1e-2 if dtype in [torch.float16, torch.bfloat16] else 1e-5
-    torch.testing.assert_close(z_torch, z_ref, rtol=tol, atol=tol)
+    if should_fail:
+       with pytest.raises(triton.TritonError, match="signedness"):
+            # we don't care about the output since it failed
+            z_placeholder = torch.empty_like(x_torch)
+            binary_kernel[grid](
+                x_ptr=x_torch, y_ptr=y_torch, z_ptr=z_placeholder,
+                n_elements=n, size=block_size, op_name=op_name, num_warps=num_warps
+            )
+    else:
+        binary_kernel[grid](
+            x_ptr=x_torch, y_ptr=y_torch, z_ptr=z_torch,
+            n_elements=n, size=block_size, op_name=op_name, num_warps=num_warps
+        )
+
+        out_dtype = z_ref.dtype
+        tol = 1e-2 if out_dtype in [torch.float16, torch.bfloat16] else 1e-5
+        torch.testing.assert_close(
+            z_torch,
+            z_ref,
+            rtol=tol,
+            atol=tol,
+            # Treats NaN == NaN as True. We're testing the output of Triton and PyTorch
+            # Not the semantics of NaN == NaN.
+            equal_nan=True
+        )
 
 if __name__ == "__main__":
     print(f"GPU: {torch.cuda.get_device_name(0)}")
