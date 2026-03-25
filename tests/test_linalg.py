ae50ce45345f5bc0ebe1a227be377bb43dac9af2
Lee Wei <lee10202013@gmail.com>
2026-03-25 15:39:05 -0600

    Test transpose

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -13,6 +13,7 @@ def matmul_kernel(
     stride_bk, stride_bn,
     stride_cm, stride_cn,
     BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, BLOCK_K: tl.constexpr,
+    A_TRANS: tl.constexpr = False,
 ):
     pid = tl.program_id(0)
 
@@ -34,12 +35,22 @@ def matmul_kernel(
         offs_k = k * BLOCK_K + tl.arange(0, BLOCK_K)
         mask_k = offs_k < K
 
-        # create 2D matrix of memory addresses
-        a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+        # Transpose turns stride access to contiguoes memory access
+        if not A_TRANS:
+            a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+            a_mask = mask_m[:, None] & mask_k[None, :]
+            a = tl.load(a_ptrs, mask=a_mask, other=0.0)
+        else:
+            a_ptrs = a_ptr + (offs_k[:, None] * stride_ak + offs_am[None, :] * stride_am)
+            a_mask = mask_k[:, None] & mask_m[None, :]
+            a = tl.load(a_ptrs, mask=a_mask, other=0.0)
+
+            # a transpose
+            a = tl.trans(a)
+
         b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
 
         # other=0.0 pads out-of-bound areas to 0.0
-        a = tl.load(a_ptrs, mask=mask_m[:, None] & mask_k[None, :], other=0.0)
         b = tl.load(b_ptrs, mask=mask_k[:, None] & mask_n[None, :], other=0.0)
         # using default Nvidia tf32 precision
         accumulator = tl.dot(a, b, acc=accumulator, out_dtype=tl.float32)
@@ -57,9 +68,10 @@ def matmul_kernel(
     N=st.integers(min_value=1, max_value=1024),
     K=st.integers(min_value=1, max_value=1024),
     num_warps=st.sampled_from(NUM_WARPS),
+    is_tran=st.booleans(),
     data=st.data()
 )
-def test_matmul_basic(M, N, K, num_warps, data):
+def test_matmul_basic(M, N, K, is_tran, num_warps, data):
     device = 'cuda'
     # Use float16 for the inputs to ensure Tensor Core usage
     dtype = torch.float16
@@ -73,16 +85,21 @@ def test_matmul_basic(M, N, K, num_warps, data):
 
     # extra padding to simulate real-world applications
     padding = data.draw(st.integers(min_value=0, max_value=32))
-    a_full = torch.randn((M, K + padding), device=device, dtype=dtype)
-    # 'a' is now logically M x K, but its stride(0) is (K + padding)
-    a = a_full[:, :K]
+    if not is_tran:
+        a = torch.randn((M, K + padding), device=device, dtype=dtype)[:, :K]
+        a_ref = a
+    else:
+        # Create (K, M) and transpose it to get an (M, K) view
+        a = torch.randn((K, M + padding), device=device, dtype=dtype)[:, :M].t()
+        a_ref = a # This is already logically (M, K)
+
     # The same for B: (K + padding) x N, sliced to K x N
     b_full = torch.randn((K + padding, N), device=device, dtype=dtype)
     b = b_full[:K, :]
 
     c_triton = torch.empty((M, N), device=device, dtype=dtype)
     # Compute reference in FP64 to get the "true" mathematical result
-    c_ref = torch.matmul(a.to(torch.float64), b.to(torch.float64)).to(dtype)
+    c_ref = torch.matmul(a_ref.to(torch.float64), b.to(torch.float64)).to(dtype)
 
     grid = (triton.cdiv(M, bm) * triton.cdiv(N, bn),)
     matmul_kernel[grid](
@@ -92,6 +109,7 @@ def test_matmul_basic(M, N, K, num_warps, data):
         b.stride(0), b.stride(1),
         c_triton.stride(0), c_triton.stride(1),
         BLOCK_M=bm, BLOCK_N=bn, BLOCK_K=bk,
+        A_TRANS=is_tran,
         num_warps=num_warps
     )
 
