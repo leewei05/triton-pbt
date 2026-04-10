e54463ca67f24a46487f3ddc92404f8164dcb4cc
Lee Wei <lee10202013@gmail.com>
2026-04-09 22:26:08 -0600

    Update init

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 module load miniforge3
-mamba create -n triton_env python=3.10 -y
+
+rm -rf ~/.conda/envs/triton_env
+
+conda config --set solver libmamba
+conda config --set channel_priority strict
+conda create -n triton_env python=3.10 -y
 
 # Use the setup script to activate and load CUDA
 source ./scripts/setup.sh
